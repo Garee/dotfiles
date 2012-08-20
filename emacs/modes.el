5adebfe09cf640703372b8cabecff664f2309073
Gary Blackwood <gareeblackwood@gmail.com>
2012-08-20 19:05:53 +0100

    Added the js2-mode package to emacs.

--- emacs/modes.el
+++ emacs/modes.el
@@ -4,6 +4,7 @@
 (add-to-list 'load-path "~/.dotfiles/emacs/packages/yasnippet")
 (add-to-list 'load-path "~/.dotfiles/emacs/packages/workgroups")
 (add-to-list 'load-path "~/.dotfiles/emacs/packages/iedit")
+(add-to-list 'load-path "~/.dotfiles/emacs/packages/js2")
 
 ;; Enable ido-mode for opening buffers.
 (ido-mode)
@@ -41,27 +42,30 @@
 (setq org-refile-targets (quote ((nil :maxlevel . 9)
                                  (org-agenda-files :maxlevel . 9))))
 (setq org-todo-keywords
-  '((sequence "TODO(t)" "IN-PROGRESS(p)" "WAITING(w@)" "|" "DONE(d!)")))
+      '((sequence "TODO(t)" "IN-PROGRESS(p)" "WAITING(w@)" "|" "DONE(d!)")))
 (setq org-tag-alist '(("University" . ?u) ("Work" . ?w) ("Home" . ?h) ("Computer" . ?l) ("Reading" . ?r)))
 (setq org-log-done 'time)
 (setq org-capture-templates
       '(("t" "Todo" entry (file "~/org/refile.org")
-	  "* TODO %?\n%U\n%a\n")
+	 "* TODO %?\n%U\n%a\n")
 	("n" "Note" entry (file "~/org/refile.org")
-	  "* %?\n%U\n%a\n")
+	 "* %?\n%U\n%a\n")
 	("i" "Idea" entry (file "~/org/refile.org")
-	  "* %?\n%U\n%a\n")
+	 "* %?\n%U\n%a\n")
 	("j" "Journal" entry (file+datetree "~/org/journal.org")
 	 "* %?\n%U\n")))
 
-
-
 (require 'iedit)
+
+;; Enhance javascript mode with js2.
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(setq js2-mirror-mode nil)
 
 ;; Enable spellchecking for .txt and .tex
 (dolist (hook '(text-mode-hook))
-      (add-hook hook (lambda () (flyspell-mode 1))))
+  (add-hook hook (lambda () (flyspell-mode 1))))
 (dolist (hook '(tex-mode-hook))
-      (add-hook hook (lambda () (flyspell-mode 1))))
+  (add-hook hook (lambda () (flyspell-mode 1))))
 
 (setq erc-hide-list '("JOIN" "PART" "QUIT"))
